c8d6478de08bbec8faa5edc46a7c101c814381ad
Ahmed Elgaidi <49118737+AhmedElgaidi@users.noreply.github.com>
2022-04-01 03:17:13 +0200

    Add files via upload

---
+++ src/redux/cakes/cakeReducer.js
@@ -0,0 +1,21 @@
+//============== Our Imports ==============
+import { BUY_CAKE } from './cakeTypes'
+
+//============== Reducers ==============
+const initialCakeState = {
+    numOfCakes: 10
+}
+
+const cakeReducer = (state = initialCakeState, action) => {
+    switch(action.type) {
+        case BUY_CAKE:
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes - 1
+            }
+        default:
+            return state;
+    }
+}
+
+export default cakeReducer;
